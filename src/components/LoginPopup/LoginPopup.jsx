635d5f089e715f15e6776987dab5d0be2ed7ea19
naga srinidhi <nagasrinidhi07@gmail.com>
2025-06-30 15:32:48 +0530

    Update LoginPopup.jsx

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,77 +1,72 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useContext, useState } from 'react'
-import './LoginPopup.css'
-import { assets } from '../../assets/assets'
-import { StoreContext } from '../../context/StoreContext'
-import axios from 'axios'
+import React, { useContext, useState } from 'react';
+import './LoginPopup.css';
+import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
-const LoginPopup = ({setShowLogin}) => {
+// eslint-disable-next-line react/prop-types
+const LoginPopup = ({ setShowLogin }) => {
+  const { setToken } = useContext(StoreContext); // Removed `url`
+  const [currState, setCurrState] = useState("Login");
 
-  const {url,setToken} = useContext(StoreContext)
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
 
-    const [currState,setCurrState] = useState("Login")
-    const [data,setData] = useState({
-      name:"",
-      email:"",
-      password:""
-    })
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  };
 
-    const onChangeHandler = (event) => {
-      const name = event.target.name;
-      const value = event.target.value;
-      setData(data=>({...data,[name]:value}))
-    }
+  const onLogin = async (event) => {
+    event.preventDefault();
 
-    const onLogin = async (event) => {
-      event.preventDefault()
-      let newUrl = url;
-      if (currState==="Login") {
-        newUrl += "/api/user/login"
+    // Use Vite proxy path directly
+    const endpoint = currState === "Login" ? "/api/user/login" : "/api/user/register";
+
+    try {
+      const response = await axios.post(endpoint, data);
+
+      if (response.data.success) {
+        setToken(response.data.token);
+        localStorage.setItem("token", response.data.token);
+        setShowLogin(false);
+      } else {
+        alert(response.data.message);
       }
-      else{
-        newUrl += "/api/user/register"
-      }
-
-    const response = await axios.post(newUrl,data);
-
-    if (response.data.success) {
-      setToken(response.data.token);
-      localStorage.setItem("token",response.data.token);
-      setShowLogin(false)
+    } catch (error) {
+      alert("Login/Register failed");
+      console.error(error);
     }
-    else{
-      alert(response.data.message)
-    }
-  }
-
-
+  };
 
   return (
     <div className='login-popup'>
       <form onSubmit={onLogin} className="login-popup-container">
         <div className="login-popup-title">
           <h2>{currState}</h2>
-          <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+          <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="close" />
         </div>
         <div className="login-popup-inputs">
-          {currState==="Login"?<></>:<input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='name' required />}
+          {currState !== "Login" && (
+            <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='name' required />
+          )}
           <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='email' required />
           <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='password' required />
         </div>
-        <button type='submit'>{currState==="Sign up"?"Create account": "Login"}</button>
+        <button type='submit'>{currState === "Sign up" ? "Create account" : "Login"}</button>
         <div className="login-popup-condition">
           <input type="checkbox" required />
-          <p>By continuing, i agree to the Terms of Use and Privacy Policy</p>
+          <p>By continuing, I agree to the Terms of Use and Privacy Policy</p>
         </div>
-        {currState==="Login"
-        ?<p>Create a new account? <span onClick={()=>setCurrState("Sign up")}>Click here</span></p>
-        :<p>Already existing account? <span onClick={()=>setCurrState("Login")}>Login here</span></p>
-        }
-
+        {currState === "Login"
+          ? <p>Create a new account? <span onClick={() => setCurrState("Sign up")}>Click here</span></p>
+          : <p>Already have an account? <span onClick={() => setCurrState("Login")}>Login here</span></p>}
       </form>
     </div>
-  )
-}
+  );
+};
 
-
-export default LoginPopup
+export default LoginPopup;
