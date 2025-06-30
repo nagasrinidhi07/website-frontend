b43306cd4d9ffe7f8b65f72e34114bf8937fbaa5
naga srinidhi <nagasrinidhi07@gmail.com>
2025-06-30 13:10:55 +0530

    Update FoodItem.jsx

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -4,21 +4,40 @@ import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 
-// eslint-disable-next-line react/prop-types
+//  Use environment variable for backend URL
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
 const FoodItem = ({ id, name, price, image }) => {
-  const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        <img className="food-item-image" src={`${url}/images/${image}`} alt={name} />
+        <img
+          className="food-item-image"
+          src={`${backendUrl}/images/${image}`}
+          alt={name}
+        />
         {!cartItems[id] ? (
-          <img className="add" onClick={() => addToCart(id)} src={assets.add_icon_white} alt="add" />
+          <img
+            className="add"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+            alt="add"
+          />
         ) : (
           <div className="food-item-counter">
-            <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="remove" />
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt="remove"
+            />
             <p>{cartItems[id]}</p>
-            <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="add more" />
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt="add more"
+            />
           </div>
         )}
       </div>
